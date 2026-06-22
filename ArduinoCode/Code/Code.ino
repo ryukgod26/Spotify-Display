60d4765044b8956982e320c2cf3c5cc75936e669
raviwhitehat <ekravi26@gmail.com>
2026-06-22 17:33:05 +0530

    Add Rotary Encoder component and update project files
    - Added a new Rotary Encoder symbol to the schematic with properties including reference, value, footprint, and description.
    - Updated the KiCad project configuration to specify the plot output directory.
    - Adjusted column widths in the project settings for better layout.

--- ArduinoCode/Code/Code.ino
+++ ArduinoCode/Code/Code.ino
@@ -12,6 +12,30 @@
 #define TFT_SCLK 4
 #define TFT_MOSI 5
 
+#define ROTATRY_CLK 10
+#define ROTATRY_DT 20
+
+int prev_btn = 6;
+int play_btn = 7;
+int next_btn = 8;
+
+bool lastPrevState = HIGH;
+bool lastPlayState = HIGH;
+bool lastNextState = HIGH;
+
+int lastClkState;
+int currentVolume = 50;
+
+unsigned long lastUpdateTime = 0;
+const unsigned long updateInterval = 2000;
+
+unsigned long lastTickTime = 0;
+long currentProgressMs = 0;
+long totalDurationMs = 240000;
+
+String lastArtist;
+String lastTrackname;
+
 char* SSID = "";
 char* PASSWORD = "";
 const char* CLIENT_ID = "";
@@ -22,6 +46,13 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RS
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(prev_btn, INPUT_PULLUP);
+  pinMode(play_btn, INPUT_PULLUP);
+  pinMode(next_btn, INPUT_PULLUP);
+
+  pinMode(ROTATRY_CLK, INPUT_PULLUP);
+  pinMode(ROTATRY_DT, INPUT_PULLUP);
 
   tft.initR(INITR_BLACKTAB);
   tft.setRotation(1);
@@ -50,6 +81,70 @@ tft.write(WiFi.localIP().toString().c_str());
 }
 
 void loop() {
+
+  bool currentPrevState = digitalRead(prev_btn);
+  bool currentPlayState = digitalRead(play_btn);
+  bool currentNextState = digitalRead(next_btn);
+
+
+  if (currentPrevState == LOW && lastPrevState == HIGH){
+    Serial.println("Button Press: Previous");
+    sp.skip_to_previous();
+    delay(200);
+  }
+  lastPrevState = currentPrevState;
+
+  if (currentPlayState == LOW && lastPlayState == HIGH){
+    Serial.println("Button Pressed: Play/Pause");
+
+    if (sp.is_playing()){
+      sp.pause_playback();
+    } 
+    // else{
+    //   // sp.res
+    // }
+    delay(200);
+  }
+  lastPlayState = currentPlayState;
+
+  if(currentNextState == LOW && lastNextState == HIGH){
+    Serial.println("Button Pressed: Next");
+    sp.skip_to_next();
+    delay(200);
+  }
+  lastNextState = currentNextState;
+
+  int clkState = digitalRead(ROTATRY_CLK);
+  if (clkState != lastClkState && clkState == LOW){
+    if (digitalRead(ROTATRY_DT) != clkState){
+      currentVolume = min(100, currentVolume + 5);
+    } else{
+      currentVolume = max(0, currentVolume - 5);
+    }
+    sp.set_volume(currentVolume);
+    delay(50);
+  }
+  lastClkState = clkState;
+
+  if(millis() - lastTickTime >= 1000){
+    lastTickTime = millis();
+    if(currentProgressMs < totalDurationMs){
+      currentProgressMs += 1000;
+
+      int maxBarWidth = 140;
+      int barX = 10;
+      int barY = 80;
+      int progressWidth = (currentProgressMs * maxBarWidth) / totalDurationMs;
+
+      tft.fillRect(barX, barY, progressWidth, 4, ST77XX_GREEN);
+      tft.fillRect(barX + progressWidth, barY, maxBarWidth - progressWidth, 4, 0x4208);
+    }
+  }
+
+
+  if(millis() - lastUpdateTime >= updateInterval){
+
+  lastUpdateTime = millis();
   String currentArtist = sp.current_artist_names();
   String currentTrackname = sp.current_track_name();
 
@@ -67,6 +162,5 @@ void loop() {
         tft.setCursor(10,40);
         tft.write(lastTrackname.c_str());
     }
-  delay(2000);
-
+  }
 }
